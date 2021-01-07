ec304c6f2f9a2db69d87f3dece104f1678f3b03d
John Sullivan <johnsully83@gmail.com>
2021-01-07 09:50:44 -0500

    Update headshot.ts
    Comments for code

--- src/app/tools/headshot/headshot.ts
+++ src/app/tools/headshot/headshot.ts
@@ -1,6 +1,16 @@
 import { AppBridgeService } from "../service/app-bridge.service";
 import {NovoElementProviders, NovoElementsModule} from 'novo-elements';
 
+
+/*
+  So the business logic here is good...I believe everything you have in typescript is roughly what it should look like logic-wise
+  
+  However this is not an Angular @Component, and thus this file wouldn't build or run.  Try poking around angular.io (e.g. https://angular.io/guide/architecture-components)
+  
+  https://github.com/johnsully83/extension-starter/blob/master/src/app/app.component.ts
+  is an example in this repository, although there isn't much going on.  Your HTML here will likely just consist of an <img /> tag
+*/
+
 public url: string;
 private readonly entityId: number;
 
